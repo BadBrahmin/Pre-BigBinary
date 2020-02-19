f75b2bec64f49b9ea0f3edcc3d99e34aeac983e6
BadBrahmin <tejasshree95@gmail.com>
2020-02-19 16:29:39 +0530

    update on changes requested

--- feature/app.js
+++ feature/app.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
-// const labelRouter = require('./routes/label');
 
 const app = express();
 
@@ -50,7 +49,6 @@ mongoose.connect(
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-// app.use('/api/v1/labels', labelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
