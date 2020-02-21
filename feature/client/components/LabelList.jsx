806bd06907427bf04b550df47106ca7d6ef03575
BadBrahmin <tejasshree95@gmail.com>
2020-02-21 11:26:49 +0530

    update cards for labels

--- feature/client/components/LabelList.jsx
+++ feature/client/components/LabelList.jsx
@@ -1,59 +1,89 @@
 import React from 'react';
-import { Container, Table, Button } from 'react-bootstrap';
+import AddNewLabel from './AddNewLabel';
+import { Container, Table, Button, Card } from 'react-bootstrap';
+import '../../public/stylesheets/style.css';
 
 class LabelList extends React.Component {
   constructor() {
     super();
     this.state = {
       labels: '',
+      displayAddNew: false,
     };
   }
 
   componentDidMount() {
-    console.log('CDM');
+    fetch('http://localhost:3000/api/v1/labels')
+      .then(res => res.json())
+      .then(labels => {
+        this.setState({ labels: labels.labels });
+      });
   }
 
+  displayAddNew = () => {
+    console.log('hit');
+    this.setState({
+      displayAddNew: !this.state.displayAddNew,
+    });
+  };
+
+  handleUpdate = () => {
+    const id = 'wherever the ID will come from';
+    const data = {
+      name: 'whatever name',
+      description: 'whatever again',
+      color: 'purr',
+    };
+
+    fetch(`http://localhost:3000/api/v1/labels/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(updatedLabel => console.log(updatedLabel));
+  };
+
+  handleDelete = () => {
+    const id = 'something something';
+
+    fetch(`http://localhost:3000/api/v1/labels/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(res => alert(res));
+  };
+
   render() {
+    const labels = this.state && this.state.labels;
+    console.log(this.state, 'labels');
+
     return (
       <Container>
-        <Table bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Label Name</th>
-              <th>Description</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* {labels &&
-              labels.map(label => {
-                <tr>
-                  <td>{label.index}</td>
-                  <td>{label.name}</td>
-                  <td>{label.description}</td>
-                  <td>
-                    <Button variant='link'>Edit</Button>
-                  </td>
-                  <td>
-                    <Button variant='link'>Delete</Button>
-                  </td>
-                </tr>;
-              })} */}
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>
-                <Button variant='link'>Edit</Button>
-              </td>
-              <td>
-                <Button variant='link'>Delete</Button>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
+        {this.state.displayAddNew ? (
+          <AddNewLabel />
+        ) : (
+          <Button variant='outline-primary' onClick={this.displayAddNew}>
+            Add New
+          </Button>
+        )}
+
+        {labels &&
+          labels.map(label => {
+            return (
+              <Card>
+                <Card.Body>
+                  <Card.Text>{label.name}</Card.Text>
+                  <Card.Text>{label.description}</Card.Text>
+                  <Card.Link href='#'>Update</Card.Link>
+                  <Card.Link href='#'>Delete</Card.Link>
+                </Card.Body>
+              </Card>
+            );
+          })}
       </Container>
     );
   }
